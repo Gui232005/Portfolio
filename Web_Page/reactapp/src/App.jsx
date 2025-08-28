22b9e14511054274ba5d9c6e549debb3b5770bd5
Gui232005 <162608929+Gui232005@users.noreply.github.com>
2025-08-28 11:13:08 +0100

    update
    I add social media, lines and left some comments to future projects

--- Web_Page/reactapp/src/App.jsx
+++ Web_Page/reactapp/src/App.jsx
@@ -3,6 +3,9 @@ import viteLogo from '/vite.svg'
 import cPlusLogo from './assets/cPlus.png'
 import flutterLogo from './assets/flutter.png'
 import cLogo from './assets/c.png'
+import gitHubLogo from './assets/github_logo.png'
+import instagramLogo from './assets/instagram.png'
+import linkedInLogo from './assets/linkedin.png'
 import './App.css'
 
 function App() {
@@ -10,18 +13,21 @@ function App() {
 
   return (
     <>
-      <div id="myFace">
+      <div>
         <img src={viteLogo} className="logo" alt="Vite logo" />
         <p id="name">𝔾𝕦𝕚𝕝𝕙𝕖𝕣𝕞𝕖 𝕊𝕚𝕝𝕧𝕒</p>
       </div>
 
       <p>
-        Hello! I'm Guilherme, a Computer Science student at the Polythecnic of Viseu. I'm passionate about technology and programming, and I'm always eager to learn new things and take on new challenges. Welcome to my portfolio!
+        Hello! I'm Guilherme, a Computer Science student at the Polythecnic Institute of Viseu. I'm passionate about technology and programming, and I'm always eager to learn new things and take on new challenges. Welcome to my portfolio!
       </p>
 
-      {/* here i will add a line to divide two parts */}
+      <br />
+      <hr style={{ border: '1px solid #888' }} />
+      <br />
 
-      <h2 style={{ textAlign: 'center', marginTop: '7em' }}>Programming languages I'm comfortable with:</h2>
+
+      <h2 style={{ textAlign: 'center', marginTop: '.5em' }}>Programming languages I'm more comfortable with:</h2>
 
       <div class="row">
         <div class="column">
@@ -44,9 +50,29 @@ function App() {
             <img src={cLogo} className="logoC" alt="C logo" style={{ alignContent: 'center' }} />
           </div>
         </div>
-
-
       </div>
+
+      
+
+      <br />
+      <hr style={{ border: '1px solid #888', marginTop: '2em' }} />
+      <br />
+
+      <div id="socialMedia">
+        <a href="https://github.com/Gui232005" target="_blank" rel="noopener noreferrer">
+          <img src={gitHubLogo} className="gitHub" alt="GitHub logo" style={{ alignContent: 'center' }} />
+        </a>
+        <a href="https://www.instagram.com/guilhermesilva_vb2005/" target="_blank" rel="noopener noreferrer">
+          <img src={instagramLogo} className="instagram" alt="Instagram logo" style={{ alignContent: 'center' }} />
+        </a>
+        <a href="https://www.linkedin.com/in/guilherme-silva-estgv/" target="_blank" rel="noopener noreferrer">
+          <img src={linkedInLogo} className="linkedIn" alt="LinkedIn logo" style={{ alignContent: 'center' }} />
+        </a>
+      </div>
+
+      <footer>
+        <p>© 2025 Guilherme Silva. All rights reserved.</p>
+      </footer>
 
     </>
   )
